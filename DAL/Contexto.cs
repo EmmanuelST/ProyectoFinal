8206493a9dc9100cb874e35f5b5883973618279d
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-07-13 14:28:29 -0400

    Agregando la entidad Usuarios

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace DAL
 {
     public class Contexto : DbContext
     {
-
+        public DbSet<Usuarios>Usuario { get; set; }
 
         public Contexto() : base("Constr")
         {
---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public int NivelUsuario { get; set; }
+
+        public Usuarios()
+        {
+            IdUsuario = 0;
+            Nombre = string.Empty;
+            Usuario = string.Empty;
+            Clave = string.Empty;
+            FechaRegistro = DateTime.Now;
+            NivelUsuario = 5;
+        }
+    }
+}
