2fff20c5c6eb46cf8610a378ec63f3fd45122b31
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-07-16 23:28:01 -0400

    Agregando la enteidad de pesador

---
+++ Entidades/Pesadores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Pesadores : Personas
+    {
+        public int IdPesador { get; set; }
+
+        public Pesadores()
+        {
+            IdPesador = 0;
+        }
+
+    }
+}
