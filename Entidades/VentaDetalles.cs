73388523877f770961b4f3dfb4aa3eca3d206759
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-07-16 23:40:13 -0400

    Agregando las entidades Ventas y VentaDetalles

---
+++ Entidades/VentaDetalles.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class VentaDetalles
+    {
+
+        public int IdVentaDetalle { get; set; }
+        public int IdVenta { get; set; }
+        public int IdProducto { get; set; } 
+        public decimal Cantidad { get; set; }   
+        public decimal Precio { get; set; }
+        public decimal SubTotal { get; set; }
+
+        public VentaDetalles()
+        {
+            IdVentaDetalle = 0;
+            IdVenta = 0;
+            IdProducto = 0;
+            Cantidad = 0;
+            Precio = 0;
+            SubTotal = 0;
+        }
+    }
+}
---
+++ Entidades/Ventas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ventas
+    {
+        public int IdVenta { get; set; }    
+        public int IdVendedor { get; set; }
+        public int IdCliente { get; set; }
+        public DateTime Fecha { get; set; } 
+        public decimal Total { get; set; }  
+        public int IdUsuario { get; set; }  
+        public int TipoVeta { get; set; }
+        public decimal TasaInteres { get; set; }
+        public DateTime HastaFecha { get; set; }
+
+        public Ventas()
+        {
+            IdVenta = 0;
+            IdVendedor = 0;
+            IdCliente = 0;
+            Fecha = DateTime.Now;
+            Total = 0;
+            IdUsuario = 0;
+            TipoVeta = 0;
+            TasaInteres = 0;
+            HastaFecha = DateTime.Now;
+        }
+    }
+}
