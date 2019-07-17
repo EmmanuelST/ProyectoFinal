377e13dd4bb714a9efd5811ee93539ffe8614f32
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-07-16 23:32:27 -0400

    Agregando la entidad Compras

---
+++ Entidades/Compras.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Compras
+    {
+        public int IdCompra { get; set; }
+        public int IdPesador { get; set; }
+        public int IdAgricultor { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Total { get; set; }
+        public int IdUsuario { get; set; }
+        public decimal PesoNeto { get; set; }
+        public decimal Humedad { get; set; }
+        public decimal TotalFanegas {get;set;}
+        public decimal PrecioFanegas { get; set; }
+
+        public Compras()
+        {
+            IdCompra = 0;
+            IdPesador = 0;
+            IdAgricultor = 0;
+            Fecha = DateTime.Now;
+            Total = 0;
+            IdUsuario = 0;
+            PesoNeto = 0;
+            Humedad = 0;
+            TotalFanegas = 0;
+            PrecioFanegas = 0;
+        }
+    }
+}
