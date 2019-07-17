6d4c2362de1ac1ac04d61c85260f80409a79f7fe
Emmanuel Suarez Taveras <manuel.413@hotmail.com>
2019-07-16 23:25:45 -0400

    Agregando la entidad Agricultor

---
+++ Entidades/Agricultores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Agricultores : Personas
+    {
+        public int IdAgricultor { get; set; }
+        public decimal Balance { get; set; }
+
+        public Agricultores()
+        {
+            IdAgricultor = 0;
+            Balance = 0;
+        }
+    }
+}
